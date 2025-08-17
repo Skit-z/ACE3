f1050745495fd65aabc0f17c8c5e8cef3ebbf7b8
Mani Gil <48371952+Skit-z@users.noreply.github.com>
2025-08-16 21:21:21 -0500

    fix merge issue + definitions

--- addons/medical/config.cpp
+++ addons/medical/config.cpp
@@ -1,13 +1,6 @@
 #include "script_component.hpp"
 
-#pragma hemtt flag pe23_ignore_has_include
-#if __has_include("\z\ace\addons\nomedical\script_component.hpp")
-#define PATCH_SKIP "No Medical"
-#endif
 
-#ifdef PATCH_SKIP
-ACE_PATCH_NOT_LOADED(ADDON,PATCH_SKIP)
-#else
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
@@ -30,4 +23,4 @@ class ACE_Tests {
     medicalHitpoints = QPATHTOF(dev\test_hitpointConfigs.sqf);
 };
 
-#endif
+
